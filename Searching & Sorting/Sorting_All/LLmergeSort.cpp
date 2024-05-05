3cb864b3a727461a9c21d77bf316fdc622251ba5
Rishabh <rgmaheshwari06@gmail.com>
2024-05-05 17:23:20 +0530

    Refactor findMid function in LLmergeSort.cpp

--- Searching & Sorting/Sorting_All/LLmergeSort.cpp
+++ Searching & Sorting/Sorting_All/LLmergeSort.cpp
@@ -20,10 +20,8 @@ public:
 
 Node* findMid(Node* head) {
     if (head == NULL || head->next == NULL) return head;
-
     Node* slow = head;
     Node* fast = head->next;
-
     while (fast != NULL && fast->next != NULL) {
         slow = slow->next;
         fast = fast->next->next;
